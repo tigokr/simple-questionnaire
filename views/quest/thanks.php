e56e81f9c6890119c78ec6c229dfdd7de63436f7
tigo <tigokr@gmail.com>
2015-10-09 23:33:40 +0300

    новое прощание

--- views/quest/thanks.php
+++ views/quest/thanks.php
@@ -6,7 +6,6 @@
  * 08.10.2015
  */
 
-use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 
 /* @var \yii\web\View $this */
@@ -20,7 +19,6 @@ $this->title = $quest->title;
 <div class="questionnaire-run">
     <h1><?= Html::encode($this->title); ?></h1>
 
-
-     <p>Спасибо, за Ваше время.</p>
+    <p>Спасибо, за то что уделили нам время! В ближайшее время мы обязательно с Вами свяжемся!</p>
 
 </div>
